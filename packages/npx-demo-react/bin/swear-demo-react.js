2e0d6c803fc340751c7020375d7b89343079b681
soundsnick <soundsnick@gmail.com>
2022-04-17 02:23:12 +0600

    fix: npx commands

--- packages/npx-demo-react/bin/swear-demo-react.js
+++ packages/npx-demo-react/bin/swear-demo-react.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 const shell = require('shelljs')
 shell.exec('git clone https://github.com/soundsnick/swear-js swear-tmp-demo-react-npx')
-shell.cd('./swear-tmp-npx/packages/demo-react')
+shell.cd('./swear-tmp-demo-react-npx/packages/demo-react')
 shell.exec('npm i')
 shell.exec('npm run start')
