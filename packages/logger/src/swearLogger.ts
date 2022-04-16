ccdba312c275720af066f261da83574c5c821f0e
soundsnick <soundsnick@gmail.com>
2022-04-17 00:54:23 +0600

    fix(logger): add dash splitter

--- packages/logger/src/swearLogger.ts
+++ packages/logger/src/swearLogger.ts
@@ -4,7 +4,7 @@ export const swearLogger = ({
   swearId, actionType, prev, payload, next,
 }: SwearPatch) => {
   const now = new Date(Date.now());
-  console.group(`🥪🧀 \x1B[33m${swearId}.${actionType}`, `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+  console.group(`🥪🧀 \x1B[33m${swearId}.${actionType}`, '-', `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
   console.log('\x1B[34mprev:', prev);
   console.log('\u001b[1;35mpayload:', payload);
   console.log('\x1B[32mnext:', next);
