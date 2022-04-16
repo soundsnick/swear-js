783d1ccc30eaf33930defd24fbd16b4a53ed4e86
soundsnick <soundsnick@gmail.com>
2022-04-17 02:11:23 +0600

    fix: npx commands

--- packages/npx-demo-agnostic/bin/swear-demo-agnostic.js
+++ packages/npx-demo-agnostic/bin/swear-demo-agnostic.js
@@ -4,5 +4,3 @@ shell.exec('git clone https://github.com/soundsnick/swear-js swear-tmp-demo-agno
 shell.cd('./swear-tmp-npx/packages/demo-agnostic')
 shell.exec('npm i')
 shell.exec('npm run start')
-shell.cd('../../../')
-shell.exec('rm -rf swear-tmp-demo-agnostic-npx')
--- packages/npx-demo-react/bin/swear-demo-react.js
+++ packages/npx-demo-react/bin/swear-demo-react.js
@@ -1,28 +1,6 @@
 #! /usr/bin/env node
-process.stdin.resume(); //so the program will not close instantly
-
 const shell = require('shelljs')
-shell.exec('git clone https://github.com/soundsnick/swear-js swear-tmp-npx')
+shell.exec('git clone https://github.com/soundsnick/swear-js swear-tmp-demo-react-npx')
 shell.cd('./swear-tmp-npx/packages/demo-react')
 shell.exec('npm i')
 shell.exec('npm run start')
-
-function exitHandler() {
-    console.log('Cleaning up...');
-    shell.cd('../')
-    shell.exec('rm -rf swear-tmp-npx')
-    process.exit()
-}
-
-//do something when app is closing
-process.on('exit', exitHandler);
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler);
-
-// catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler);
-process.on('SIGUSR2', exitHandler);
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler);
