9c1004a1ae58bd707620033b402dcc47c592abcc
soundsnick <soundsnick@gmail.com>
2022-04-17 07:31:58 +0600

    fix(logger): delete dash

--- packages/logger/src/swearLogger.ts
+++ packages/logger/src/swearLogger.ts
@@ -5,7 +5,7 @@ export const swearLogger = (logger: ISwearLogger = console) => ({
   swearId, actionType, prev, payload, next,
 }: SwearPatch) => {
   const now = new Date(Date.now());
-  logger.group(`🥪🧀 \x1B[33m${swearId}.${actionType}`, '-', `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+  logger.group(`🥪🧀 \x1B[33m${swearId}.${actionType}`, `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
   logger.log('\x1B[34mprev:', prev);
   logger.log('\u001b[1;35mpayload:', payload);
   logger.log('\x1B[32mnext:', next);
