1c094058b125647f9a6bf7cbf3c9788ef415e108
soundsnick <soundsnick@gmail.com>
2022-04-17 07:31:07 +0600

    feat: add interactivity to release script

--- release.js
+++ release.js
@@ -8,9 +8,8 @@ if (code !== 0) {
     console.log("\x1B[32m[1] Compilation success! Publishing...\x1b[0m");
 
     const publish = child_process.spawnSync("lerna", ["publish"], {stdio: 'inherit'});
-    console.log(publish.status)
     if (publish.status !== 0) {
-        console.error("\x1B[31mRelease failed! Lerna publish failed/canceled\x1b[0m", e);
+        console.error("\x1B[31mRelease failed! Lerna publish failed/canceled\x1b[0m");
     } else {
         console.log("\x1B[32m[1] Publishing success! Pushing chore commit\x1b[0m");
         shell.exec("git push origin main");
