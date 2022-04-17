e098d02051fd2bd9638201fa2d7d8de88a6b65b5
soundsnick <soundsnick@gmail.com>
2022-04-17 07:13:57 +0600

    feat: add info messages to release script

--- release.js
+++ release.js
@@ -4,10 +4,12 @@ const { code } = shell.exec('npm run compile', { async: false });
 if (code !== 0) {
     console.error("\x1B[31mRelease canceled! Compilation failed");
 } else {
+    console.log("\x1B[35m[1] Compilation success! Publishing...");
     const { publishCode } = shell.exec("lerna publish");
     if (publishCode !== 0) {
         console.error("\x1B[31mRelease failed! Lerna publish failed/canceled");
     } else {
+        console.log("\x1B[35m[1] Publishing success! Pushing chore commit");
         shell.exec("git push origin main");
     }
 }
